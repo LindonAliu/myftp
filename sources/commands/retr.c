6f5996dc3a99dca1236e600f7fbe4c7a8d175c18
Lindon ALIU <lindon.aliu@epitech.eu>
2023-03-08 18:09:07 +0100

    feat: add retr function and stor error handling

--- sources/commands/retr.c
+++ sources/commands/retr.c
@@ -8,18 +8,52 @@
 #include "server.h"
 #include "all_lib.h"
 #include "builtins_array.h"
+#include <stdlib.h>
+
+static int error_handling_retro(const char **cmd,
+    struct server *server, int index)
+{
+    if (server->clients[index]->a.connected < 2) {
+        dprintf(server->clients[index]->cfd, code_530);
+        return -1;
+    }
+    if (my_len_array(cmd) != 2) {
+        dprintf(server->clients[index]->cfd, code_501);
+        return -1;
+    }
+    if (server->clients[index]->m.type == NONE) {
+        dprintf(server->clients[index]->cfd, code_425);
+        return -1;
+    }
+    return 0;
+}
+
+static void print_data_in_fd(int fd, FILE *f)
+{
+    char *line = NULL;
+    size_t len = 0;
+
+    while (getline(&line, &len, f) != -1)
+        dprintf(fd, "%s", line);
+    if (line)
+        free(line);
+}
 
 int retr(const char **cmd, struct server *server, int index)
 {
-    if (my_len_array(cmd) != 2) {
-        dprintf(server->clients[index]->cfd, code_501);
+    FILE *f = 0;
+    
+    if (error_handling_retro(cmd, server, index) == -1)
         return 0;
-    }
-    if (server->clients[index]->a.connected < 2) {
-        dprintf(server->clients[index]->cfd, code_530);
+    f = fopen(cmd[1], "r");
+    if (f == NULL) {
+        dprintf(server->clients[index]->cfd, code_550);
         return 0;
     }
     dprintf(server->clients[index]->cfd, code_150);
+    print_data_in_fd(server->clients[index]->m.fd, f);
+    pclose(f);
+    destroy_mode(&server->clients[index]->m);
     dprintf(server->clients[index]->cfd, code_226);
     return 0;
 }
--- sources/commands/stor.c
+++ sources/commands/stor.c
@@ -9,16 +9,28 @@
 #include "all_lib.h"
 #include "builtins_array.h"
 
-int stor(const char **cmd, struct server *server, int index)
+static int error_handling_stor(const char **cmd,
+    struct server *server, int index)
 {
-    if (my_len_array(cmd) != 2) {
-        dprintf(server->clients[index]->cfd, code_501);
-        return 0;
-    }
     if (server->clients[index]->a.connected < 2) {
         dprintf(server->clients[index]->cfd, code_530);
-        return 0;
+        return -1;
     }
+    if (my_len_array(cmd) > 2) {
+        dprintf(server->clients[index]->cfd, code_501);
+        return -1;
+    }
+    if (server->clients[index]->m.type == NONE) {
+        dprintf(server->clients[index]->cfd, code_425);
+        return -1;
+    }
+    return 0;
+}
+
+int stor(const char **cmd, struct server *server, int index)
+{
+    if (error_handling_stor(cmd, server, index) == -1)
+        return -1;
     dprintf(server->clients[index]->cfd, code_150);
     dprintf(server->clients[index]->cfd, code_226);
     return 0;
