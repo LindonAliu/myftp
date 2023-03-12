e2426657c5b88907eb44750f8b29ad935fa86093
Lindon ALIU <lindon.aliu@epitech.eu>
2023-03-12 13:37:21 +0100

    fix: error handling in retr before accept

--- sources/commands/retr.c
+++ sources/commands/retr.c
@@ -45,10 +45,7 @@ static void print_data_in_fd(int fd, int fd_data)
 
 static void do_retr(int fd, int fd_data, struct server *server, int index)
 {
-    if (fd_data == -1) {
-        dprintf(server->clients[index]->cfd, code_550);
-        return;
-    }
+    dprintf(server->clients[index]->cfd, code_150);
     print_data_in_fd(fd, fd_data);
     close(fd_data);
     close(fd);
@@ -56,13 +53,30 @@ static void do_retr(int fd, int fd_data, struct server *server, int index)
     dprintf(server->clients[index]->cfd, code_226);
 }
 
+static int open_file(const char *path, struct server *server, int index)
+{
+    char *new_path = NULL;
+    int fd = 0;
+
+    asprintf(&new_path, "%s/%s", server->clients[index]->working_dir, path);
+    fd = open(new_path, O_RDONLY);
+    free(new_path);
+    if (fd == -1) {
+        dprintf(server->clients[index]->cfd, code_550);
+        return -1;
+    }
+    return fd;
+}
+
 int retr(const char **cmd, struct server *server, int index)
 {
     int fd_data = 0;
     int fd = 0;
-    char *path = NULL;
 
     if (error_handling_retr(cmd, server, index) == -1)
+        return 0;
+    fd_data = open_file(cmd[1], server, index);
+    if (fd_data == -1)
         return 0;
     fd = server->clients[index]->m.type != ACTIVE ?
         accept(server->clients[index]->m.sfd, NULL, NULL) :
@@ -71,10 +85,6 @@ int retr(const char **cmd, struct server *server, int index)
         dprintf(server->clients[index]->cfd, code_425);
         return 0;
     }
-    dprintf(server->clients[index]->cfd, code_150);
-    asprintf(&path, "%s/%s", server->clients[index]->working_dir, cmd[1]);
-    fd_data = open(path, O_RDONLY);
-    free(path);
     do_retr(fd, fd_data, server, index);
     return 0;
 }
